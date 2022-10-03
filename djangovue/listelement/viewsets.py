7f3f6732b7400e27bcbcf47c9edb3f43b1247f14
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2022-10-03 14:47:11 +0200

    Seguimos trabajando con resk framework

--- djangovue/listelement/viewsets.py
+++ djangovue/listelement/viewsets.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Element,Category,Type
 from .serializer import ElementSerializer,CategorySerializer,TypeSerializer
@@ -10,9 +12,19 @@ class ElementViewSet(viewsets.ModelViewSet):
 class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
+    @action(detail=True, methods=['get'])
+    def elements(self, request, pk=None):
+        queryset = Element.objects.filter(category_id=pk)
+        serializer = ElementSerializer(queryset, many=True)
+        return Response(serializer.data)
 
 
 class TypeViewSet(viewsets.ModelViewSet):
     serializer_class = TypeSerializer
     queryset = Type.objects.all()
+    @action(detail=True, methods=['get'])
+    def elements(self, request, pk=None):
+        queryset = Element.objects.filter(type_id=pk)
+        serializer = ElementSerializer(queryset, many=True)
+        return Response(serializer.data)
     
