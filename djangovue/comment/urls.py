57abe3cb6f8e8ee2abe482fd8cc8483105a007c2
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2022-10-04 15:35:49 +0200

    Seguimos con la creación del formulario

--- djangovue/comment/urls.py
+++ djangovue/comment/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import add, index
+from .views import add, index, update
 
 urlpatterns = [
     path('', index, name="indexcomment"),
     path('add', add, name="addcomment"),
+    path('update/<int:pk>', update, name="updatecomment"),
 ]
--- djangovue/comment/views.py
+++ djangovue/comment/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Comment
 from .forms import CommentForm
 
@@ -11,8 +11,31 @@ def index(request):
 
 
 def add(request):
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('indexcomment')
+    else:
+        form = CommentForm()
 
-    form = CommentForm()
     return render(request,'comment/add.html', {
         'form':form
+    })
+
+def update(request, pk):
+
+    comment = get_object_or_404(Comment, pk=pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('updatecomment',pk=comment.id)
+    else:
+        form = CommentForm(instance=comment)
+
+    return render(request,'comment/update.html', {
+        'form':form,
+        'comment':comment
     })
