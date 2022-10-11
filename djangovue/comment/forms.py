957dc58d16c9fcaea484432d6dcd10924da62494
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2022-10-11 15:26:10 +0200

    Seguimos trasteando con formularios

--- djangovue/comment/forms.py
+++ djangovue/comment/forms.py
@@ -19,4 +19,7 @@ class CommentForm(ModelForm):
 from django import forms 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label="Nombre", required=True, disabled=True, help_text="Aquí va tu nombre", max_length=10, min_length=2)
+    name = forms.CharField(label="Nombre", required=True, disabled=False, help_text="Aquí va tu nombre", max_length=10, min_length=2)
+    surname = forms.CharField(label="Apellido", required=False , max_length=10, min_length=2)
+    phone = forms.RegexField(label='Telefono', regex='\(\w{3}\)\w{3}-\w{4}')
+    date_birth = forms.DateField(label='Fecha de nacimiento')
