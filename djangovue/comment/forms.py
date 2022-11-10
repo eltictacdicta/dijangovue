6957367ba1417a74c87b1c8c7d20313d142d1458
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2022-11-10 20:54:22 +0100

    Empezamos a trabajar con Widgets

--- djangovue/comment/forms.py
+++ djangovue/comment/forms.py
@@ -17,9 +17,14 @@ class CommentForm(ModelForm):
             instance.save()
 
 from django import forms 
+from django.core.validators import MinLengthValidator,EmailValidator
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label="Nombre", required=True, disabled=False, help_text="Aquí va tu nombre", max_length=10, min_length=2)
+    #name = forms.CharField(label="Nombre", required=True, disabled=False, help_text="Aquí va tu nombre", max_length=10, min_length=2)
+    name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}) ,label="Nombre", validators=[MinLengthValidator(2, message="Muy corto! (minimo %(limit_value)d) actual %(show_value)d")])
     surname = forms.CharField(label="Apellido", required=False , max_length=10, min_length=2)
-    phone = forms.RegexField(label='Telefono', regex='\(\w{3}\)\w{3}-\w{4}')
-    date_birth = forms.DateField(label='Fecha de nacimiento')
+    #phone = forms.RegexField(label='Telefono', regex='\(\w{3}\)\w{3}-\w{4}')
+    phone = forms.CharField(label='Telefono')
+    date_birth = forms.DateField(widget=forms.DateInput(),label='Fecha de nacimiento')
+    email = forms.EmailField(label="Correo")
+    #mail = forms.CharField(label="Email", validators=[EmailValidator("No es un email válido")])
