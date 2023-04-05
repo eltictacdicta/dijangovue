006a812a18794a18b269ff100dd212fedfb4b4b4
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2023-04-05 13:27:33 +0200

    Empezamos a trabajar con el modulo de autenticación

--- djangovue/djangovue/urls.py
+++ djangovue/djangovue/urls.py
@@ -5,5 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('listelement.urls')),
-    path('comment/',include('comment.urls') )
+    path('comment/',include('comment.urls') ),
+    path("accounts/", include("django.contrib.auth.urls"))
 ]
