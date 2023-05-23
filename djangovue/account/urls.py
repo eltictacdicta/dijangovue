316144a48e6afd068d48c8dec6626a4085b626ec
Javier Trujillo <javier.trujillo.jimenez@gmail.com>
2023-05-23 18:37:56 +0200

    Empezamos a proteger recursos con login

--- djangovue/account/urls.py
+++ djangovue/account/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from .views import user_data
+from .views import user_data,profile
 app_name='comment'
 urlpatterns = [
     path('user_data/', user_data, name="user_data"),
+    path('profile/', profile, name="profile"),
 ]
--- djangovue/account/views.py
+++ djangovue/account/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def user_data(request):
     print(request.user.username)
+    return render(request,'account/user_data.html')
+
+@login_required
+def profile(request):
     return render(request,'account/user_data.html')
